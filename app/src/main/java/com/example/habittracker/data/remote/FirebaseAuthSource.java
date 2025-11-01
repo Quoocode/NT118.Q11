fd2f81e77aae1243119f58e66b1a92ed8f32b0e5
truongdaoanhduy <truongdaoanhduy@gmail.com>
2025-11-02 00:04:08 +0700

    authen

--- app/src/main/java/com/example/habittracker/data/remote/FirebaseAuthSource.java
+++ app/src/main/java/com/example/habittracker/data/remote/FirebaseAuthSource.java
@@ -1,4 +1,30 @@
 package com.example.habittracker.data.remote;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class FirebaseAuthSource {
+    private final FirebaseAuth firebaseAuth;
+
+    public FirebaseAuthSource() {
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public Task<AuthResult> register(String email, String password) {
+        return firebaseAuth.createUserWithEmailAndPassword(email, password);
+    }
+
+    public Task<AuthResult> login(String email, String password) {
+        return firebaseAuth.signInWithEmailAndPassword(email, password);
+    }
+
+    public void logout() {
+        firebaseAuth.signOut();
+    }
+
+    public FirebaseUser getCurrentUser() {
+        return firebaseAuth.getCurrentUser();
+    }
 }
--- app/src/main/java/com/example/habittracker/data/repository/AuthRepository.java
+++ app/src/main/java/com/example/habittracker/data/repository/AuthRepository.java
@@ -1,4 +1,30 @@
 package com.example.habittracker.data.repository;
 
+import com.example.habittracker.data.remote.FirebaseAuthSource;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AuthRepository {
+    private final FirebaseAuthSource authSource;
+
+    public AuthRepository() {
+        this.authSource = new FirebaseAuthSource();
+    }
+
+    public Task<AuthResult> register(String email, String password) {
+        return authSource.register(email, password);
+    }
+
+    public Task<AuthResult> login(String email, String password) {
+        return authSource.login(email, password);
+    }
+
+    public void logout() {
+        authSource.logout();
+    }
+
+    public FirebaseUser getCurrentUser() {
+        return authSource.getCurrentUser();
+    }
 }
